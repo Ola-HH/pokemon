ad933d46d7d71006be25a139a8f7c69921b4e0c1
Ola Hagerupsen Hansen <96570269+Ola-HH@users.noreply.github.com>
2023-10-02 16:15:12 +0200

    Main parts of the project

--- pokemon-app/src/App.tsx
+++ pokemon-app/src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import PokeTable from './components/PokeTable';
+import { Grid, Typography } from '@mui/material';
+import PokemonInfo from './components/PokemonInfo';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#121212',
+      paper: '#1e1e1e'
+    }
+  },
+});
 
 function App() {
+  const [selected, setSelected] = useState<string | null>();
+  const select = (name: string | null) => setSelected(name);
+
+  window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+  }, false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Typography variant='h3' padding={1}>PokeApp</Typography>
+      <Grid container spacing={3} padding={3}>
+        <Grid item sm={12} md={6}>
+          <PokeTable onSelection={select} />           
+        </Grid>
+        <Grid item sm={12} md={6}>
+          <PokemonInfo name={selected} />          
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
